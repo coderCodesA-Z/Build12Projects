5ee7c9ef541ca7ed14f426233915d118290ec879
Sayan Dutta <rockistuart@gmail.com>
2023-01-20 18:02:57 +0530

    on click button, feedback added

--- FeedBackUI/script.js
+++ FeedBackUI/script.js
@@ -1,5 +1,6 @@
 const ratingElements = document.querySelectorAll(".rating");
 const btnEl = document.getElementById("btn");
+const containerEl = document.getElementById("container");
 
 let selectedRating = "";
 
@@ -12,7 +13,22 @@ ratingElements.forEach(ratingElement => {
         event.target.parentNode.classList.add("active");
         
     })
-})
+});
+
+btnEl.addEventListener("click", () => {
+    if(selectedRating == "") return;
+    let message = "";
+    if(selectedRating === "UnHappy") message = "We're Sorry to hear that; If there's something we can do for you, please let us know!";
+    else if(selectedRating === "Neutral") message = "We hope to see you again. We're constantly improving through your feedbacks!";
+    else message = "Thanks so much for taking the time to leave us a Satisfied Feedback – it’s much appreciated!"
+    containerEl.innerHTML = `
+    <strong>ThankYou!&hearts;</strong>
+    <br><br>
+    <strong>Feedback: ${selectedRating}</strong>
+    <br><br>
+    <p>${message}</p>
+    `;
+});
 
 function removeActive() {
     ratingElements.forEach(ratingElement => {
