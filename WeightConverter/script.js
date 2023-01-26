1742901467d054989099a4f60ae4664dc1e134fe
Sayan Dutta <rockistuart@gmail.com>
2023-01-27 00:10:37 +0530

    completed

--- WeightConverter/script.js
+++ WeightConverter/script.js
@@ -4,23 +4,22 @@ const resEl = document.getElementById("copmputed-weight");
 
 let errorTime, clearRes;
 function updateRes() {
-    const inputElVal = +inputEl.value;
-    if(inputElVal < 0 || isNaN(inputElVal)) {
-        errorEl.innerText = "Enter a Valid Number!";
-        clearTimeout(errorTime);
-        errorTime = setTimeout(()=>{
-            errorEl.innerText = "";
-            inputEl.value = "";
-        },2000);
-    } else {
-        resEl.innerText = (inputElVal/2.2).toFixed(2);
-        clearTimeout(clearRes);
-        clearRes = setTimeout(()=>{
-            inputEl.value = "";
-            resEl.innerText = "";
-        },7000)
-    }
+	const inputElVal = +inputEl.value;
+	if (inputElVal < 0 || isNaN(inputElVal)) {
+		errorEl.innerText = "Enter a Valid Number!";
+		clearTimeout(errorTime);
+		errorTime = setTimeout(() => {
+			errorEl.innerText = "";
+			inputEl.value = "";
+		}, 2000);
+	} else {
+		resEl.innerText = (inputElVal / 2.2).toFixed(2);
+		clearTimeout(clearRes);
+		clearRes = setTimeout(() => {
+			inputEl.value = "";
+			resEl.innerText = "";
+		}, 7000);
+	}
 }
-
 
 inputEl.addEventListener("input", updateRes);
