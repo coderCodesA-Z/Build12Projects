33ef9977d2475b0bf6c0727ff93de59b55d39739
Sayan Dutta <rockistuart@gmail.com>
2023-01-27 00:03:12 +0530

    completed

--- WeightConverter/script.js
+++ WeightConverter/script.js
@@ -2,7 +2,7 @@ const inputEl = document.getElementById("input");
 const errorEl = document.getElementById("error");
 const resEl = document.getElementById("copmputed-weight");
 
-   let errorTime;
+let errorTime, clearRes;
 function updateRes() {
     const inputElVal = +inputEl.value;
     if(inputElVal < 0 || isNaN(inputElVal)) {
@@ -13,7 +13,12 @@ function updateRes() {
             inputEl.value = "";
         },2000);
     } else {
-        resEl.innerText = inputElVal/2.2;
+        resEl.innerText = (inputElVal/2.2).toFixed(2);
+        clearTimeout(clearRes);
+        clearRes = setTimeout(()=>{
+            inputEl.value = "";
+            resEl.innerText = "";
+        },7000)
     }
 }
 
